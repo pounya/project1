a5f96a3c40dc131c384e60fd4d7518e792fb9423
pounya <127486287+pounya@users.noreply.github.com>
2023-03-10 15:48:57 +0530

    Add files via upload

---
+++ project1.java
@@ -0,0 +1,8 @@
+package com.testingautomation;
+
+public class project1 {
+    public static void main(String[] args) {
+
+        System.out.println("WElCOME TO TESTING AUTOMATION CLASSES");
+    }
+}
