586a5319abd949c2b34467f9b371805b6a114b23
pounya <127486287+pounya@users.noreply.github.com>
2023-03-10 16:08:16 +0530

    Add files via upload

---
+++ additionoftwonums.java
@@ -0,0 +1,11 @@
+public class additionoftwonums
+{ public static void main(String[] args)
+{
+    int x = 40;
+    int y = 270;
+
+    int sum = x + y;
+    System.out.println(x + " + " + y + " = "  + sum);
+}
+}
+
